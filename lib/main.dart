9231be1ae9ecf472cc11e9fcacb3ec9c17256ddd
Joshua Obritsch <j.obritsch@gmail.com>
2019-09-13 17:25:21 +0200

    Add floating action button

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 void main() {
   var app = MaterialApp(
     home: Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          print('Hi there!');
+        },
+      ),
       appBar: AppBar(
         title: Text('Title'),
       ),
