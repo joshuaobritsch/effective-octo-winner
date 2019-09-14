97d7d2287ebae7ca22c62de2504588b8be071d50
Joshua Obritsch <j.obritsch@gmail.com>
2019-09-14 16:09:29 +0200

    Add decorations to image list

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -10,8 +10,27 @@ class ImageList extends StatelessWidget {
     return ListView.builder(
       itemCount: images.length,
       itemBuilder: (BuildContext context, int index) {
-        return Text(images[index].url);
+        return buildImage(images[index]);
       },
+    );
+  }
+
+  Widget buildImage(ImageModel image) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+      ),
+      padding: EdgeInsets.all(20.0),
+      margin: EdgeInsets.all(20.0),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            child: Image.network(image.url),
+            padding: EdgeInsets.only(bottom: 8.0),
+          ),
+          Text(image.title),
+        ],
+      ),
     );
   }
 }
